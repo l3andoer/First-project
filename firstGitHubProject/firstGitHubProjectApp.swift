25d8c9c1f4b805e2b796b298c742aee1df676489
adrian <aabreu64@haverfordsd.net>
2024-04-18 11:22:33 -0400

    Initial Commit

---
+++ firstGitHubProject/firstGitHubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstGitHubProjectApp.swift
+//  firstGitHubProject
+//
+//  Created by Adrian Abreu 2025 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct firstGitHubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
